2767e84fd95da7a56965c07a43c0b32d04fe627f
William Moore <w.moore@dundee.ac.uk>
2022-10-04 14:48:38 +0100

    Add script for downloading commands

---
+++ scripts/s3_sync.py
@@ -0,0 +1,36 @@
+
+# Run python s3_sync.py to output command for downloading to ftp server
+
+# $ ssh -A idr-ftp.openmicroscopy.org
+# $ cd /data/idrftp-incoming/idr0125-way-cellpainting/
+# USE TODAY's date
+# $ mkdir s3-20220927
+# $ cd s3-20220927/
+# $ conda activate aws
+
+# Then run this script locally...
+# $ cd idr0125-way-cell-painting
+# $ python scripts/s3_sync.py
+
+# This will output 2 commands. The first does the download.
+# the 2nd command 
+
+plates_yml = "docs/_data/plates.yml"
+command = """aws s3 sync --no-sign-request --exclude '*' --include "*.z*" --include "*.xml" s3://cellpainting-gallery/cpg0004-lincs/broad/images/2016_04_01_a549_48hr_batch1/images_zarr/$a $a"""
+
+first_plate_index = 40
+batch_size = 10
+
+plates = []
+with open(plates_yml) as f:
+    plates = [line.replace("- ", "").strip() for line in f.readlines()]
+
+plate_names = " ".join(plates[first_plate_index: first_plate_index + batch_size])
+
+# $ for a in SQ00014812__2016-05-23T20_44_31-Measurement1.ome.zarr SQ00014813__2016-05-23T19_03_28-Measurement1.ome.zarr; do mkdir $a; aws s3 sync --no-sign-request --exclude '*' --include "*.z*" --include "*.xml" s3://cellpainting-gallery/cpg0004-lincs/broad/images/2016_04_01_a549_48hr_batch1/images_zarr/$a $a; done
+print('for a in %s; do mkdir $a; %s; done' % (plate_names, command))
+print("")
+print('for a in %s; do find ./$a/ -name ".zarray" | wc; done' % plate_names)
+
+# SQ00015096__2016-06-08T17_05_23-Measurement1.ome.zarr
+# SQ00015097__2016-06-08T15_26_27-Measurement1.ome.zarr
