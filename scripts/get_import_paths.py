09a7f9639a873eae38f3a3c56f28765c40c713ad
William Moore <w.moore@dundee.ac.uk>
2022-11-21 12:18:09 +0000

    Symlinking scripts

---
+++ scripts/get_import_paths.py
@@ -0,0 +1,24 @@
+
+import requests
+
+SERVER = "http://localhost:1080/"
+
+plates_url = SERVER + "webclient/api/plates/?id=3052"
+
+plates_json = requests.get(plates_url).json()
+
+for plate in plates_json["plates"]:
+
+    wells_url = SERVER + f"webgateway/plate/{plate['id']}/0/"
+    wells_json = requests.get(wells_url).json()
+    first_img_id = wells_json["grid"][0][0]['id']
+
+    files_url = SERVER + f"webgateway/original_file_paths/{first_img_id}/"
+    # print("files_url", files_url)
+    files_json = requests.get(files_url).json()
+
+    first_path = files_json["repo"][0]
+    # "demo_2/Blitz-0-Ice.ThreadPool.Server-11/2022-10/06/09-46-37.995/SQ00014812__2016-05-23T20_44_31-Measurement1.ome.zarr/H/13/0/.zattrs"
+    path_to_plate = first_path.split("ome.zarr")[0]
+
+    print(path_to_plate)
---
+++ scripts/symlink_cmd.py
@@ -0,0 +1,40 @@
+
+# Run python symlink_cmd.py to output command for symlinking imported plates
+# from managed repo to s3-mount.
+
+# sudo -u omero-server -s
+
+# cd /data/OMERO/ManagedRepository/demo_2/Blitz-0-Ice.ThreadPool.Server-28/2022-10/15/05-53-40.405
+# rm -rf SQ00015233__2016-05-12T05_47_03-Measurement1.ome.zarr
+# ln -s /cellpainting-gallery/cpg0004-lincs/broad/images_zarr/2016_04_01_a549_48hr_batch1/images/SQ00015233__2016-05-12T05_47_03-Measurement1.ome.zarr SQ00015233__2016-05-12T05_47_03-Measurement1.ome.zarr
+
+file_path = "imported_paths.txt"
+batch1_dir = "/cellpainting-gallery/cpg0004-lincs/broad/images/2016_04_01_a549_48hr_batch1/images_zarr/"
+batch2_dir = "/cellpainting-gallery/cpg0004-lincs/broad/images/2016_04_01_a549_48hr_batch1/images_zarr_050/"
+
+paths = []
+with open(file_path) as f:
+    paths = [path.strip() for path in f.readlines()]
+
+# paths = paths[30:]
+
+# print("sudo -u omero-server -s")
+
+for line in paths:
+    import_dir = line.split("/SQ0")[0]
+    plate_name = "SQ0" + line.split("/SQ0")[1]
+    s3_plates_dir = batch1_dir
+    # later import of regenerated plates 
+    if "2022-11" in import_dir:
+        s3_plates_dir = batch2_dir
+
+    print(f"cd /data/OMERO/ManagedRepository/{import_dir}")
+    print(f"rm -rf {plate_name}")
+    print(f"ln -s {batch1_dir}{plate_name} {plate_name}")
+
+
+for line in paths:
+    # ls /A/1/well/resolution/t/c/z/y -> should give chunks 0 1 2
+    import_dir = line.split("/SQ0")[0]
+    plate_name = "SQ0" + line.split("/SQ0")[1]
+    print(f"ls /data/OMERO/ManagedRepository/{import_dir}/{plate_name}/A/1/0/0/0/0/0/0/")
